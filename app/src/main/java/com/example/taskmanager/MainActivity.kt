6df6fbfcf1fbd84765dc796971b721903c01e0b1
DarshanaBombuwala <navodhya123@gmail.com>
2024-09-07 18:22:50 +0530

    Home page welcome message and background image

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -3,19 +3,24 @@ package com.example.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color= MaterialTheme.colorScheme.background) {
-
+                        WelcomeImage()
                 }
             }
         }
@@ -36,26 +41,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WelcomeText(modifier: Modifier = Modifier){
-    Text(
-        text = "This productivity tool is designed to help " +
-                "you better manager your task " +
-                "project-wise conveniently"
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier=modifier) {
+        Text(
+            text = "Task Management",
+            fontSize=30.sp,
+            lineHeight = 40.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(20.dp)
+
+        )
+        Text(
+            text = "This productivity tool is designed to help " +
+                    "you better manager your task " +
+                    "project-wise conveniently",
+            fontSize = 20.sp,
+            lineHeight = 21.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+
+}
+
+@Composable
+fun WelcomeImage(modifier: Modifier=Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+
+    Box(modifier){
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.3F
+        )
+        WelcomeText(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
 }
 
 @Preview(
-    showBackground = true,
-    showSystemUi = true
 )
 @Composable
 fun WelcomeTextPreview(modifier: Modifier = Modifier){
     TaskManagerTheme {
-        Text(
-            text = "This productivity tool is designed to help " +
-                    "you better manager your task " +
-                    "project-wise conveniently"
-        )
+        WelcomeImage()
     }
-
 }
 
